36565078dd3226864640d059066785893090a11a
sheikhabdullahahmed <sheikhabdullahahmed>
2024-02-15 13:15:14 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var num1 =4;
+var num2 =6;
+var add=num1+num2;
+var sub=num1-num2;
+var mul=num1*num2;
+var div=num1/num2;
+document.write(`addition of two number is ${num1} and ${num2} ${add}<br>
+subraction of two is ${num1} and ${num2} ${sub}<br>
+multiplication of two is ${num1} and ${num2} ${mul}<br>
+division of two is ${num1} and ${num2} ${div}
+
+`)
